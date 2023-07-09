d7c74c75c12dbb7dc5ab20fb95f5844c6879797f
karthik1131 <rangana.dinesh09@gmail.com>
2023-07-09 19:38:31 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Component/Nav/Navbar';
+import Intro from '../src/Component/Intro/Intro'
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      <Navbar />
+      <Intro/>
     </div>
   );
 }
